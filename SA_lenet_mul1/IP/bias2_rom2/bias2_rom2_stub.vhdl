ca149d005abf2599369ac85cc7d13706f6c46227
NinjaV999 <114104075+NinjaV999@users.noreply.github.com>
2024-11-14 08:47:05 +0100

    Add files via upload

---
+++ SA_lenet_mul1/IP/bias2_rom2/bias2_rom2_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Wed Oct 23 12:29:30 2024
+-- Host        : NinjaV-PC running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               d:/HuaweiMoveData/Users/kiwi/Desktop/SA_lenet/project_1/project_1.srcs/sources_1/ip/bias2_rom2/bias2_rom2_stub.vhdl
+-- Design      : bias2_rom2
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity bias2_rom2 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 1 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end bias2_rom2;
+
+architecture stub of bias2_rom2 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[1:0],douta[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_4,Vivado 2019.2";
+begin
+end;
---
+++ SA_lenet_mul1/IP/data_rom17/data_rom17_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Tue Nov 12 16:22:41 2024
+-- Host        : NinjaV-PC running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               D:/HuaweiMoveData/Users/kiwi/Desktop/SA_lenet_mul/project_1/project_1.srcs/sources_1/ip/data_rom17_1/data_rom17_stub.vhdl
+-- Design      : data_rom17
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity data_rom17 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 8 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end data_rom17;
+
+architecture stub of data_rom17 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[8:0],douta[0:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_4,Vivado 2019.2";
+begin
+end;
